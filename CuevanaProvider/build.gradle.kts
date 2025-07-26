39cd3a95a3cdb6d1985f572c2e535e12db3639d2
rodcod24-droid <rodcod24@gmail.com>
2025-07-25 20:06:27 -0700

    Update build.gradle.kts

--- CuevanaProvider/build.gradle.kts
+++ CuevanaProvider/build.gradle.kts
@@ -1,36 +1,48 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
-    id("com.recloudstream.gradle")
-}
-
-version = 1
-
-cloudstream {
-    language = "es"
-    description = "Cuevana streaming provider for Spanish content"
-    authors = listOf("YourName")
-    status = 1
-    tvTypes = listOf("TvSeries", "Movie")
-    iconUrl = "https://www.google.com/s2/favicons?domain=wv5n.cuevana.biz&sz=%size%"
-    requiresResources = false
 }
 
 android {
     compileSdk = 34
+    namespace = "com.lagradost"
+    
     defaultConfig {
         minSdk = 21
         targetSdk = 34
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
     }
 }
 
 dependencies {
     implementation("com.github.recloudstream:cloudstream:pre-release")
+}
+
+// Custom task to create CS3 file
+tasks.register<Copy>("makeCS3") {
+    dependsOn("assembleRelease")
+    
+    from("$buildDir/outputs/apk/release") {
+        include("*.apk")
+        rename { "CuevanaProvider.cs3" }
+    }
+    into("$rootDir/outputs")
+    
+    doFirst {
+        file("$rootDir/outputs").mkdirs()
+    }
 }
