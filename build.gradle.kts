081e39a764667fe852aea9cc2b443afe6cbde820
rodcod24-droid <rodcod24@gmail.com>
2025-07-25 20:05:56 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // Try with a specific commit or tag instead of master-SNAPSHOT
-        classpath("com.github.recloudstream:gradle:latest-SNAPSHOT")
     }
 }
 
