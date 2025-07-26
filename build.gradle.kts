4628cd6cc5d146498b9d73dc470087b93ac8ad61
rodcod24-droid <rodcod24@gmail.com>
2025-07-25 19:35:23 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        // Remove the CloudStream plugin for now
+        // Add the CloudStream plugin back
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
     }
 }
 
