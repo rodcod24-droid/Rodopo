1cda0dad1dab217572d1b6a1a4d40fa1f4a26a90
rodcod24-droid <rodcod24@gmail.com>
2025-07-27 16:46:43 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,6 @@ buildscript {
     }
 }
 
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
-
 allprojects {
     repositories {
         google()
