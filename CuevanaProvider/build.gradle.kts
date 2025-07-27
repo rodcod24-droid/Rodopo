b342530584edcb80ad2a5f7981581867d9d57441
rodcod24-droid <rodcod24@gmail.com>
2025-07-27 16:47:23 -0700

    Update build.gradle.kts

--- CuevanaProvider/build.gradle.kts
+++ CuevanaProvider/build.gradle.kts
@@ -1,44 +1,31 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-}
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
 
 android {
     compileSdk = 34
     namespace = "com.lagradost"
-    
+
     defaultConfig {
         minSdk = 21
         targetSdk = 34
-        
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
-    
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-    
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
 }
 
 dependencies {
     implementation("com.github.recloudstream:cloudstream:pre-release")
-    
-    // Add these if not already included by cloudstream
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
